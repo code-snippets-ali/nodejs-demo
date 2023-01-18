f075cf311748ee61a38c504d812dfcb99037bef8
Ali Zafar <ali.zaffar@gmail.com>
2023-01-17 19:41:19 -0500

    improved response from middleware auth

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,36 +1,60 @@
 import { Request, Response } from "express";
+import { HttpStatusCode } from "../serviceobjects/Error";
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const { get } = require("config");
 
 function auth(req: Request, res: Response, next: Function) {
     const token = req.header("x-auth-token");
-    if (!token) return res.status(400).send("Access denied. No token provided");
+    if (!token) {
+    }
+    return res.status(HttpStatusCode.BAD_REQUEST).send({
+        success: false,
+        message: "Access denied. Please provide an access token",
+    });
     try {
         const decoded = jwt.verify(token, config.get("jwtPrivateKey"));
         req.body.user = decoded;
         next();
     } catch (ex) {
         console.log(ex);
-        res.status(401).send("Invalid token");
+        res.status(HttpStatusCode.UNAUTHORIZED).send("Invalid token");
     }
 }
 
 function admin(req: Request, res: Response, next: Function) {
-    if (!req.body.user.isAdmin) return res.status(403).send("Access Denied");
+    if (!req.body.user.isAdmin) {
+        return res.status(HttpStatusCode.UNAUTHORIZED).send({
+            success: false,
+            message: "Access denied. You are not authorized to access this api",
+            statusCode: HttpStatusCode.UNAUTHORIZED,
+        });
+    }
     next();
 }
 
 function refresh(req: Request, res: Response, next: Function) {
     const refreshToken = req.body.refreshToken;
-    if (!refreshToken) return res.status(400).send("Refresh Token required");
+    if (!refreshToken) {
+        return res.status(HttpStatusCode.BAD_REQUEST).send({
+            success: false,
+            message:
+                "Access denied. Please provide the refresh token to proceed",
+            statusCode: HttpStatusCode.BAD_REQUEST,
+        });
+    }
+
     try {
         const decoded = jwt.verify(refreshToken, config.get("jwtPrivateKey"));
         req.body.user = decoded;
         next();
     } catch (ex) {
         console.log(ex);
-        res.status(400).send("Invalid token");
+        res.status(HttpStatusCode.BAD_REQUEST).send({
+            success: false,
+            message: "Access denied. Refresh token is invalid",
+            statusCode: HttpStatusCode.BAD_REQUEST,
+        });
     }
 }
 
