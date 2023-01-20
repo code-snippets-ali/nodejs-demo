dc9986d3934d7ef587d0a2389d3fcfddc9708d71
Ali Zafar <ali.zaffar@gmail.com>
2023-01-20 18:29:30 -0500

    converted to typescript for consistency

--- startup/unhandledExceptions.ts
+++ startup/unhandledExceptions.ts
@@ -1,13 +1,13 @@
 const { logger } = require("../serviceobjects/Utilities/logger");
 
 module.exports = function () {
-    process.on("uncaughtException", (ex) => {
+    process.on("uncaughtException", (ex: any) => {
         console.log("Here is uncaught exception");
         logger.error(ex.message, {
             metadata: { Type: "Unhandled Exception" },
         });
     });
-    process.on("unhandledRejection", (ex) => {
+    process.on("unhandledRejection", (ex: any) => {
         console.log("Here is uncaught exception");
         logger.error(ex.message, {
             metadata: { Type: "Unhandled Exception" },
