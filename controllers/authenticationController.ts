20338d0381600e34585c0823ad1e6e541989012d
Ali Zafar <ali.zaffar@gmail.com>
2023-08-16 10:34:19 -0400

    controllers added

---
+++ controllers/authenticationController.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import { IResponse } from "../serviceobjects/Interfaces/IResponse";
+import { AuthenticationService } from "../serviceobjects/authenticationService";
+
+export interface IAuthenticationResponse extends IResponse {
+    accessToken?: string;
+    refreshToken?: String;
+    expiresIn?: String;
+}
+
+export async function register(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const service = new AuthenticationService();
+
+        const authentication: IAuthenticationResponse =
+            await service.registerUser(req.body);
+
+        res.status(authentication.statusCode)
+            .header("x-auth-token", authentication.accessToken ?? "")
+            .send(authentication);
+    } catch (ex: any) {
+        next(ex);
+    }
+}
+
+export async function signin(req: Request, res: Response, next: NextFunction) {
+    const service = new AuthenticationService();
+    const authentication: IAuthenticationResponse = await service.signIn(
+        req.body
+    );
+    res.status(authentication.statusCode)
+        .header("x-auth-token", authentication.accessToken ?? "")
+        .send(authentication);
+}
+
+export async function token(req: Request, res: Response, next: NextFunction) {
+    const service = new AuthenticationService();
+    const authentication: IAuthenticationResponse = await service.refreshToken(
+        req.body.user
+    );
+    res.header("x-auth-token", authentication.accessToken ?? "").send(
+        authentication
+    );
+}
--- routes/users.ts
+++ routes/users.ts
@@ -1,30 +1,11 @@
 import express from "express";
 import { UserService } from "../serviceobjects/UserService";
+import { getProfile, updateProfile } from "../controllers/userController";
 const { auth } = require("../middleware/auth");
 const router = express.Router();
 
-router.get("/me", auth, async (req, res, next) => {
-    try {
-        const service = new UserService();
-        const profile = await service.me(req.body.user._id);
-        res.send(profile);
-    } catch (ex: any) {
-        next(ex);
-    }
-});
+router.get("/me", auth, getProfile);
 
-router.patch("/me", auth, async (req, res, next) => {
-    try {
-        const service = new UserService();
-        let result = await service.updateProfile(req.body);
-        if (result.success) {
-            res.status(result.statusCode).send();
-        } else {
-            res.status(result.statusCode).send(result);
-        }
-    } catch (ex: any) {
-        next(ex);
-    }
-});
+router.patch("/me", auth, updateProfile);
 
 module.exports = router;
