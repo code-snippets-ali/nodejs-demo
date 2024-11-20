2cece23761ebbd3bb4eeb71e3ea16130bedd10b8
Ali Zafar <ali.zaffar@gmail.com>
2024-11-19 19:43:47 -0500

    Minor tweaks to stay consistent with production app

--- startup/routes.ts
+++ startup/routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
 
+// SECTION: Middleware
 app.use(require("cors")());
 app.use(express.json());
 app.use(express.static("public"));
 
+//SECTION: Routes
 app.use("/api/users", require("../routes/users"));
 app.use("/api/authenticate", require("../routes/authenticate"));
 app.use("/api/doctors", require("../routes/doctors"));
