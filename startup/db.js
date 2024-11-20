c3553ea6ae2e07b93ec85d0d3c94d6c6f97ceb03
Ali Zafar <ali.zaffar@gmail.com>
2024-11-20 15:45:08 -0500

    fixing warning

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const { appConfig, Settings } = require("../serviceobjects/Utilities/Settings");
 module.exports = function (app) {
+    mongoose.set("strictQuery", false);
     mongoose
         .connect(appConfig(Settings.DBString))
         .then(() => console.log("Connected to MongoDB..."));
